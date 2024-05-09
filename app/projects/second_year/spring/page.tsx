5b03dd6b0dbfd8fbad1301b79f6cced532f59697
ori raisfeld <raisfeldori@gmail.com>
2024-05-09 08:47:23 +0300

    finished second year

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -1,33 +1,28 @@
-import example from './example_img.jpg';
+import menu from './menu.png';
+import example from './example.png';
 import Image from "next/image"
 import Link from "next/link";
 export default function Page(){
   return <div className="container">
   <h1 className="head">Ksed</h1>
   <p className="description">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-    voluptas quae quasi amet dolorum aliquid minus quaerat error 
-    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-    voluptas quae quasi amet dolorum aliquid minus quaerat error 
-    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+    Using all of the skills I had learned so far, I decided to make an app based on
+    rd-security and raisfeld encrypter called "Ksed". Ksed is an app with a
+    desktop environment feel that allows you to have a hidden, alternative
+    desktop that is more secure. I worked on both the frontend and the backend
+    of the app, and I created most of the main page and almost all of the backend.
+    This is my biggest project so far, and I hope to advance it further and turn it into
+    a full on business.
   </p>
   <div className="images">
     <div className="image-container">
-      <Image className="image" src={example} alt=""/>
+      <Image className="image" src={menu} alt=""/>
     </div>
     <div className="image-container">
       <Image className="image" src={example} alt=""/>
     </div>
     
     <ul className="linksprj">
-  
-  <li className="linkprj">
-    <Link href={"#"}>LinkedIn</Link>
-  </li>
-  <li className="linkprj"><Link href={"#"}>Github</Link></li>
-  <li className="linkprj"><Link href={"#"}>Facebook</Link></li>
 </ul>
     
   </div>
--- app/projects/second_year/summer/page.tsx
+++ app/projects/second_year/summer/page.tsx
@@ -1,33 +1,30 @@
-import example from './example_img.jpg';
+import example from './example.png';
+import login from './login.png';
 import Image from "next/image"
 import Link from "next/link";
 export default function Page(){
   return <div className="container">
-  <h1 className="head">Project name</h1>
+  <h1 className="head">Ksed | Tiod</h1>
   <p className="description">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-    voluptas quae quasi amet dolorum aliquid minus quaerat error 
-    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-    voluptas quae quasi amet dolorum aliquid minus quaerat error 
-    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+    Unlike what I expected, the summer project was very small. So what did we do? well, we did 2 things,
+    the first, more small thing, was to add settings to Ksed, so you can actually edit things like the
+    background image or the volume. The larger thing started in a hackathon, we made a concept for an app 
+    named Tiod that won the hackathon and earned 3,000₪ for the project! we decided to continue the project
+    this summer and our goal is to hopefully, but the start of the next year, already release a demo.
   </p>
   <div className="images">
     <div className="image-container">
       <Image className="image" src={example} alt=""/>
     </div>
     <div className="image-container">
-      <Image className="image" src={example} alt=""/>
+      <Image className="image" src={login} alt=""/>
     </div>
     
     <ul className="linksprj">
   
   <li className="linkprj">
-    <Link href={"#"}>LinkedIn</Link>
+    <Link href={"https://drive.google.com/file/d/1XEKrJXFnhgQBMH5KW_xrKkur3gqrpLje/view?usp=sharing"}>tiod's video</Link>
   </li>
-  <li className="linkprj"><Link href={"#"}>Github</Link></li>
-  <li className="linkprj"><Link href={"#"}>Facebook</Link></li>
 </ul>
     
   </div>
