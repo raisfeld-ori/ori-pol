6298a271088bbb283b6ad35036b5f03c1ae87732
ori raisfeld <raisfeldori@gmail.com>
2024-05-12 10:31:57 +0300

    redesigned the CV

--- app/projects/about/page.tsx
+++ app/projects/about/page.tsx
@@ -5,7 +5,6 @@ export default function socials(){
   
   return <div className="container">
     <h1 className="head">About me</h1>
-    <div className="descriptions">
     <div className="aboutme">
      <h1 className="Whoami">Who am i?</h1>
     <p className="description">
@@ -17,27 +16,12 @@ export default function socials(){
         when i met this school, I saw this as an opportunity to finaly learn how to program instead
         of doing the same things I learned 2 years ago, with 0 passion. with this, i also have my hobby, 
         Jidokwan, a korean martial art that was made by korea and japan togather, 
-        which later became taekwando and karate. this is long to explain, so i just prefer 
-        to say i do karate instead. besides this, i also have 2 sisters, noya and naama, noya is 14 years old and 
-        is intrested in art, and naama is 9 years old and is intrested in youtube.
+        which later became taekwando and karate.
     </p>
     </div>
     <div className="CV">
-      <h1 className="CV-head">CV</h1>
-      <p className="CV-text">
-        I started programming last year, with no prior knowlage. In fact, in the start of the first year,
-        I started by looking up &apos;python&apos; (the only programming language I knew) into youtube and seeing what could
-        I find. Throughout the entire start of my career, I had 0 knowlage of what to do, but I think this is a good
-        thing. because I didn&apos;t know anything, I ended up using the absulote worse tools for the job, in every job,
-        which forced me to spend hours reserching and learning how to solve most issues I come accross. Because of
-        this weird path, I ended up as a fullstack developer with the skills to completly make an app from scratch,
-        using the languages python, rust, javascript/typescript and whatever tool I thought would fit for the job.
-        I also tried to challange myself every once in a while with a difficult challange in something I am not familiar
-        with, like game development in c#, making AI models with tensorflow and I even made compilers with C, python and rust.
-        I also had a couple projects that were not included, mainly: a PDF editor that I showed in google, replacement
-        to chamama&apos;s website, habit tracker, gfilte and a CS website for our group in school.
-      </p>
-    </div>
+      <h1 className="CV-head">CV:</h1>
+      <h2 className="CV-minor">example</h2>
     </div>
     <ul className="links">
   
