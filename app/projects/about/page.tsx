f3c7da775e47fbe33de536bb2691095fe6064a61
ori raisfeld <raisfeldori@gmail.com>
2024-05-12 11:15:37 +0300

    changed CV

--- app/projects/about/page.tsx
+++ app/projects/about/page.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 export default function socials(){
   
   return <div className="container">
-    <h1 className="head">About me</h1>
+    <h1 className="head">Quick CV:</h1>
+    <p className="description">My name is Ori, I live in hacarmel 70 kfar sava, and I focus on the subject of fullstack, and I have been programming for 2 years</p>
     <div className="aboutme">
-     <h1 className="Whoami">Who am i?</h1>
+     <h1 className="Whoami">Who am I?</h1>
     <p className="description">
-        Hi! my name is ori raisfeld, and i&apos;m a student at the highschool chamama.
+        I&apos;m a student at the highschool chamama.
         I first tried learning how to program first a couple. years ago, but after 
         around a month or so of trying and enjoying HTML, I was stopped by
         things required by school (tests, homework etc). even though i did fail, this gave
@@ -20,15 +21,16 @@ export default function socials(){
     </p>
     </div>
     <div className="CV">
-      <h1 className="CV-head">CV:</h1>
       <h2 className="CV-minor">skills:</h2>
-      <p className="CV-text">fullstack (5/5), rust (4/5), python (5/5), react (5/5), react native (3/5), html/css/javascript (4/5), 
-      game development (2/5), data science (3/5), computer science (2/5), frontend (3/5), backend (4/5), databases (5/5), servers (3/5)
+      <p className="CV-text">fullstack (5/5), rust (4/5), python (5/5), react (4/5), react native (3/5), html/css/javascript (4/5), 
+      game development (2/5), data science (3/5), computer science (2/5), frontend (3/5), backend (4/5), databases (5/5), servers (3/5),
+      Sveltekit (2/5), Figma/UX (3/5), Qt (3/5), leading a team (4/5)
       </p>
       <h2 className="CV-minor">Education:</h2>
-      <p className="CV-text">while most of my knowlage was made with experiece, I also learned using:
-      open-university, free code camp and youtube channels. I also finished CS-50, Campus-il courses 
-      (in C, cyber security and computer science), I'm also doing a degree at the Open university in computer science.
+      <p className="CV-text">I learned using the Open-university, free code camp and youtube channels. 
+      I also finished CS-50, Campus-il courses  (in C, cyber security and computer science), 
+      I'm also doing a degree at the Open university in computer science. But, most of my learning has been self taught,
+      meaning I found something that I consider interesting, learn the basics of how it works, and then spend a long time
       </p>
     </div>
     <ul className="links">
